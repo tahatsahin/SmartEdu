2ea1a9b313b84b45ef68e113ce17ca465f2a16f2
Taha <tahatsahin@gmail.com>
2022-11-02 12:09:29 +0300

    mvc structure

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 import express from 'express';
+import ejs from 'ejs';
 
 const app = express();
 
-const PORT = 8080;
+// template engine
+app.set('view engine', 'ejs');
 
+// middleware
+app.use(express.static('public'));
+
+// routing
 app.get('/', (req, res) => {
-	res.send('index page');
+	res.status(200).render('index', {
+		pageName: 'index',
+	});
+});
+app.get('/about', (req, res) => {
+	res.status(200).render('about', {
+		pageName: 'about',
+	});
 });
 
+const PORT = 8080;
 app.listen(PORT, () => {
 	console.log(`start server at port ${PORT}`);
 });
